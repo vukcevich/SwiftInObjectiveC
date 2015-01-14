9392025dde8660412a6744b57c4e3ccdf2dfadd8
vukcevich <marijan@eastsideinteractive.com>
2015-01-14 14:09:50 -0800

    Initial Commit

---
+++ SwiftInObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwiftInObjectiveC
+//
+//  Created by Marijan Vukcevich on 1/14/15.
+//  Copyright (c) 2015 xyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SwiftInObjectiveC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SwiftInObjectiveC
+//
+//  Created by Marijan Vukcevich on 1/14/15.
+//  Copyright (c) 2015 xyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
